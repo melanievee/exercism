8c93c436fffe8881ec4eb662ff486e4cfd119e3a
Melanie <melanie.vanderlugt@gmail.com>
2015-01-30 14:15:07 +0100

    Use a local method instead of constant to define gigasecond value.

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,6 +1,7 @@
 class Gigasecond
-	GIGASECOND = 10**9
+	def self.gigasecond() 10**9 end
+	
 	def self.from(date)
-		(date.to_time + GIGASECOND).to_date
+		(date.to_time + gigasecond).to_date
 	end
 end
