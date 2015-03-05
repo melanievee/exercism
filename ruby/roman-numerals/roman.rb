2580ed8ea854ab96ad23c644604af9305fcb7322
Melanie <melanie.vanderlugt@gmail.com>
2015-03-05 17:30:28 +0100

    Trying out reduce per comment from amar47shah.

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,15 +1,13 @@
 class Fixnum
   def to_roman
     roman_pairs = {"M"=>1000, "CM"=>900, "D"=>500, "CD"=>400, "C"=>100, "XC"=>90, "L"=>50, "XL"=>40, "X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1}
+    roman_string = ""
 
-    roman = ""
-    temp_number = self
-
-    roman_pairs.each do | key, value |
-      roman << key * (temp_number / value)
-      temp_number = temp_number % value
+    roman_pairs.reduce(self) do | temp_number, roman_pair |
+      roman_string << roman_pair.first * (temp_number / roman_pair.last)
+      temp_number % roman_pair.last
     end
 
-    roman
+    roman_string
   end
 end
