8fca5d87936a4b2a22dd0774021b61151ce9d1cc
Melanie <melanie.vanderlugt@gmail.com>
2015-03-09 15:44:19 +0100

    Streamline using reduce.

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,10 @@
 class Hamming
 	def self.compute(string1, string2)
 		string1.length == string2.length or raise "Input Strings should be the same length."
-		distance = 0
-		(0..string1.length-1).each do |i|
-			distance += 1 if string1[i] != string2[i]
-		end
-		distance
+
+    (0..string1.length-1).reduce(0) do | distance, n |
+      (string1[n] != string2[n]) ? distance+1 : distance
+    end
+
 	end
 end
