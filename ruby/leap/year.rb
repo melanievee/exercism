db3ac8368def0ba2204003731ae8f15b764ed78d
Melanie <melanie.vanderlugt@gmail.com>
2015-03-13 17:00:15 +0100

    Further refactor to eliminate any explicit return calls.

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,6 +1,6 @@
 class Year
   def self.leap?(year)
-    return true if year.modulo(400).zero?
-    year.modulo(4).zero? && year.modulo(100).nonzero?
+    year.modulo(400).zero? || ( year.modulo(4).zero? && !year.modulo(100).zero? )
   end
 end
+
