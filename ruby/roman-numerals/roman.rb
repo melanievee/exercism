519fe0f5f2ce670272ae4a0856556cba40979691
Melanie <melanie.vanderlugt@gmail.com>
2015-03-13 16:35:04 +0100

    Refactor reduce to pass an array on each iteration.

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,13 +1,11 @@
 class Fixnum
   def to_roman
     roman_pairs = {"M"=>1000, "CM"=>900, "D"=>500, "CD"=>400, "C"=>100, "XC"=>90, "L"=>50, "XL"=>40, "X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1}
-    roman_string = ""
 
-    roman_pairs.reduce(self) do | temp_number, roman_pair |
-      roman_string << roman_pair.first * (temp_number / roman_pair.last)
-      temp_number % roman_pair.last
+    output = roman_pairs.reduce([self,""]) do | (remainder, roman_string), (rpair_char, rpair_num) |
+      [ remainder % rpair_num, roman_string << rpair_char * (remainder / rpair_num) ]
     end
 
-    roman_string
+    output.last
   end
 end
