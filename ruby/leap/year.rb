b61c9e017907a2c4265ca54fff9d1b017ea60bcb
Melanie <melanie.vanderlugt@gmail.com>
2015-03-19 17:56:07 +0100

    Swapped out modulo for percent and zero? for ==0.

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,5 +1,5 @@
 class Year
   def self.leap?(year)
-    year.modulo(400).zero? || ( year.modulo(4).zero? && !year.modulo(100).zero? )
+    year % 400 == 0 || year % 4 == 0 && year % 100 != 0
   end
 end
