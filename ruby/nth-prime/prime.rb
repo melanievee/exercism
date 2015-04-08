756ef258360919046ca9a2caf64a8486ac95edce
Melanie <melanie.vanderlugt@gmail.com>
2015-04-08 09:45:41 +0200

    Use class variable to track existing primes and prevent re-work. Streamlined loop to count and add primes.

--- ruby/nth-prime/prime.rb
+++ ruby/nth-prime/prime.rb
@@ -1,15 +1,23 @@
 class Prime
+  class << self
+    attr_accessor :primes
+  end
+
+  @primes = [2,3]
+
   def self.nth(num)
     num > 0 or raise ArgumentError, "nth input cannot be less than 1"
     
-    primecount = 1
-    i = 2
-
-    loop do 
-      return i if primecount == num
-      i += 1
-      primecount += 1 if i.prime?
+    return @primes[num-1] if num <= @primes.count
+      
+    i = @primes.last+2
+    while @primes.count < num
+      @primes << i if i.prime?
+      i += 2
     end
+
+    @primes.last
+    
   end
 end
 
