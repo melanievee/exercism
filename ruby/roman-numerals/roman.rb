ba4735e7845df522b9c20ab13e514298d1d11d96
Melanie <melanie.vanderlugt@gmail.com>
2015-04-10 10:59:17 +0200

    Use divmod to improve readability.

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,9 +1,12 @@
 class Fixnum
   def to_roman
-    roman_pairs = {"M"=>1000, "CM"=>900, "D"=>500, "CD"=>400, "C"=>100, "XC"=>90, "L"=>50, "XL"=>40, "X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1}
+    roman_pairs = {"M"=>1000, "CM"=>900, "D"=>500, "CD"=>400, 
+                   "C"=>100, "XC"=>90, "L"=>50, "XL"=>40, 
+                   "X"=>10, "IX"=>9, "V"=>5, "IV"=>4, "I"=>1}
 
     output = roman_pairs.reduce([self,""]) do | (remainder, roman_string), (rpair_char, rpair_num) |
-      [ remainder % rpair_num, roman_string << rpair_char * (remainder / rpair_num) ]
+      quotient, modulus = remainder.divmod(rpair_num)
+      [ modulus, roman_string << rpair_char * quotient ]
     end
 
     output.last
