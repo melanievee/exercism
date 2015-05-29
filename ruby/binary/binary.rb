ef73b95adf132a0e2644919e76ffbc913598ffc5
Melanie <melanie.vanderlugt@gmail.com>
2015-05-29 14:58:32 -0700

    Initial binary commit

---
+++ ruby/binary/binary.rb
@@ -0,0 +1,19 @@
+class Binary
+  def initialize(binary)
+    @binary = binary
+  end
+
+  def to_decimal
+    return 0 if @binary =~ /[^01]/
+    number = index = 0
+    @binary.reverse.each_char do |digit|
+      number += digit.to_i * 2**index
+      index += 1
+    end
+    number
+  end
+
+  def preprocess(binary)
+
+  end
+end
